51080d81a5ab12d32f3f65cf921a25474c6a7e07
Rashmi <rashmichigon65@gmail.com>
2022-11-21 11:04:16 +0530

    EmployeeWageUC9

---
+++ EmployeeWageBuilderObject.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmployeeWageBuilderObject
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHourPerMonth;
+        private int totalEmpWage;
+
+        public EmployeeWageBuilderObject(string company, int empRatePerHour,
+         int numOfWorkingDays,
+         int maxHourPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHourPerMonth = maxHourPerMonth;
+        }
+
+        public void computeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= this.maxHourPerMonth && totalWorkingDays < this.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company:" + company + "is:" + totalEmpWage);
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company:" + company + "is:" + totalEmpWage;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,42 +1,23 @@
-﻿namespace EmployeeWage
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmployeeWage
 {
     internal class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static int computeEmpWage()
-        {
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-                totalWorkingDays++;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs += empHrs;
-            Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
-        }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        Console.WriteLine("TotalEmpWage :" + totalEmpWage);
-return totalEmpWage
-}
-     static void Main(String[] args)
-    {
 
-        computeEmpWage("Dmart", 20, 2, 10);
-        computeEmpWage("Reliance", 10, 4, 20);
+        static void Main(String[] args)
+        {
+            EmployeeWageBuilderObject dMart = new EmployeeWageBuilderObject("Dmart", 20, 2, 10);
+            EmployeeWageBuilderObject reliance = new EmployeeWageBuilderObject("reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+        }
     }
 }
+  
